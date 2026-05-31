e2297ce12e38b0e6ae85d49eeb1e649243f2e938
Jochen Schalanda <jochen@schalanda.name>
2026-06-01 00:01:02 +0200

    feat: update nixpkgs to nixos-26.05 (#63)
    https://nixos.org/blog/announcements/2026/nixos-2605/

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Nix container for usage with Forgejo Actions";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   };
 
   outputs =
